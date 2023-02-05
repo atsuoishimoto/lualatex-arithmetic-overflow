bce617780afd3f2582ee6af23729145a50db5e15
ishimoto <atsuoishimoto@gmail.com>
2023-02-05 22:55:20 +0900

    init

---
+++ sphinx-noerror/source/conf.py
@@ -0,0 +1,20 @@
+project = 'test1'
+copyright = '2023, a'
+author = 'a'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ja'
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+latex_engine="lualatex"
+
+latex_docclass = {
+'manual': 'ltjsbook',
+'howto': 'ltjsarticle'
+}
